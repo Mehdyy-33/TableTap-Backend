ee62bff05486236cd249702f9b1cdff5ce9507fb
Kaylinhh <ly.julie@live.fr>
2023-06-06 16:58:03 +0200

    pulling dev

--- src/main/java/manager/tabletap/Category/CategoryRepository.java
+++ src/main/java/manager/tabletap/Category/CategoryRepository.java
@@ -1,8 +1,15 @@
 package manager.tabletap.Category;
 
+import manager.tabletap.Subcategory.Subcategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+
+
 }
