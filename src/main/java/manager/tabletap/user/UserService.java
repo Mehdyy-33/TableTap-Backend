2b0f1d6cf558ed23e757a52f69a758bb618d1672
Kaylinhh <ly.julie@live.fr>
2023-06-20 16:59:27 +0200

    fixed product

--- src/main/java/manager/tabletap/user/UserService.java
+++ src/main/java/manager/tabletap/user/UserService.java
@@ -2,7 +2,9 @@ package manager.tabletap.user;
 
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 
@@ -12,6 +14,9 @@ public class UserService {private final UserRepository userRepository;
         this.userRepository = userRepository;
     }
 
+    public Optional<User> getUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
     public List<User> getAll() {
         return userRepository.findAll();
     }
@@ -19,6 +24,7 @@ public class UserService {private final UserRepository userRepository;
     public User getById (Long id) {
         return userRepository.findById(id).orElseThrow(()-> new RuntimeException(id + "not found"));
     }
+
     public void delete(Long id) {
         userRepository.deleteById(id);
     }
