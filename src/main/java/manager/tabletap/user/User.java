0d9b2a99fa0d5b17763a350de259c57af768e5c5
Kaylinhh <ly.julie@live.fr>
2023-06-21 12:20:03 +0200

    joined products to user

--- src/main/java/manager/tabletap/user/User.java
+++ src/main/java/manager/tabletap/user/User.java
@@ -38,18 +38,22 @@ public class User implements UserDetails {
     private String numberTable;
 
     @OneToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinColumn(name="user_id", referencedColumnName = "id")
     @JsonIgnore
     private Set<Product> products = new HashSet<>();
 
     @OneToMany
+    @JoinColumn(name="user_id", referencedColumnName = "id")
     @JsonIgnore
     private Set<PromoCode> promoCodes = new HashSet<>();
 
     @OneToMany
+    @JoinColumn(name="user_id", referencedColumnName = "id")
     @JsonIgnore
     private Set<SalesFigure> salesFigures = new HashSet<>();
 
     @OneToMany
+    @JoinColumn(name="user_id", referencedColumnName = "id")
     @JsonIgnore
     private Set<ProductCount> productCounts = new HashSet<>();
 
